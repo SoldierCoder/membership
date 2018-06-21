5eaecd25c3d0947445f6c55c96a4480d10354997
Ed Drain <geekprogrammer.ed@gmail.com>
2018-06-20 23:02:26 -0700

    initial commit

---
+++ test/canary.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+describe("Canary", function(){
+  describe("How rich smells the air", function() {
+    it("is pungent", function(){
+      assert.ok(true);
+    });
+  });
+});
